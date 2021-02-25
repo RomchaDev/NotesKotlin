e6847ae0e5b5b5f3a973197cb36bd9232b13dfe4
Romeo2005 <ua.roma2005@gmail.com>
2021-02-25 15:57:13 +0200

    Deleted unnecessary files

--- app/src/main/java/org/romeo/noteskotlin/splash/SplashViewState.kt
+++
@@ -1,6 +0,0 @@
-package org.romeo.noteskotlin.splash
-
-import org.romeo.noteskotlin.base.BaseViewState
-
-class SplashViewState(isAuth: Boolean = false, error: Throwable? = null)
-    : BaseViewState<Boolean>(isAuth, error)
